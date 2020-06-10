ddd6822283d021feb2e88c2f015dec354424b597
Kunwar Vidhan <51852048+kunwarvidhan@users.noreply.github.com>
2020-06-10 12:45:55 +0530

    Add files via upload

---
+++ reducer (1).py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import sys
+
+#Total = 0
+mini= 10000000000.0
+X = ""
+for line in sys.stdin:
+    data = line.strip().split()
+    fun, x = data
+    fun = float(fun)
+    if mini > fun:
+        mini = fun
+        X = x
+    
+print (mini,X)
